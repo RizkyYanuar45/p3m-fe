01747e7ad3ca39bb5fe307551bb0238a0f5f8773
RizkyYanuar45 <rizkyanuar4@gmail.com>
2025-08-19 10:04:19 +0700

    push main

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
+  server: {},
 });
